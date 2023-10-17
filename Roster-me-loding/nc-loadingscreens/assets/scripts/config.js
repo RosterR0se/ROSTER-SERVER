3f3fa6ebdd39ee32f7b57ae488875eecd53be33f
Roster <148140350+RosterR0se@users.noreply.github.com>
2023-10-17 21:13:55 +0530

    RS

---
+++ Roster-me-loding/nc-loadingscreens/assets/scripts/config.js
@@ -0,0 +1,30 @@
+Config = {}; // Don't touch
+
+Config.ServerIP = "YOUR IP SERVER";
+
+// Social media buttons on the left side
+Config.Socials = [
+    {name: "discord", label: "Discord", description: "Click here to copy the link and join our Discord server!", icon: "assets/media/icons/discord.png", link: "https://discord.gg/Q92nMpQNuN"},
+    {name: "instagram", label: "Instagram", description: "An Instagram page will open for us soon, feel free to join and follow us!", icon: "assets/media/icons/tiktok.png", link: "#"},
+    {name: "tebex", label: "Donations", description: "For donations, feel free to look at the room - #Donations at Discord.", icon: "assets/media/icons/tebex.png", link: "#"},
+];
+
+Config.HideoverlayKeybind = 112 // JS key code https://keycode.info
+Config.CustomBindText = "F1"; // leave as "" if you don't want the bind text in html to be statically set
+
+// Staff list
+Config.Staff = [
+    {name: "ripson013", description: "Owner & Dev", color: "#ff0000", image: "https://cdn.discordapp.com/attachments/1017474751016489021/1115183340660719759/73a17da7d0f892b57412d3cbc9c58bb55b3315b79e3e1d4a6f2d2767a3752dc3.png"},
+    {name: "roster_0", description: "Owner", color: "#ff0000", image: "https://cdn.discordapp.com/attachments/1079327590227136554/1163860432247869471/Roster.png"},
+
+
+];
+
+// Categories
+Config.Categories = [
+    {label: "Social Media", default: true},
+    {label: "Staff", default: false}
+];
+
+// Music
+Config.Song = "song.mp3";
